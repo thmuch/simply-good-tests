e447227b8ced6ab0a3e43cfd256dcfac36330254
Thomas Much <github@snailshell.de>
2021-10-05 14:42:13 +0200

    Replace @BeforeEach method by explicit calls to factory methods

--- src/test/java/sushi/service/SushiServiceTest.java
+++ src/test/java/sushi/service/SushiServiceTest.java
@@ -9,29 +9,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class SushiServiceTest {
 
-    private Customer customer;
-    private Menu menu;
-
-    @BeforeEach
-    void setup() {
-        customer = new Customer();
-
-        customer.setId(1234L);
-        customer.setName("Thomas");
-
-        menu = new Menu();
-
-        menu.setName("Maki");
-    }
-
     @Test
     void orderMenuShouldReturnValidOrderNumber() {
 
         SushiService sushiService = new SushiService();
 
+        Customer customer = defaultCustomer();
+        Menu menu = makiMenu();
+
         long orderNumber = sushiService.orderMenu(customer, menu);
 
         assertThat(orderNumber).isGreaterThanOrEqualTo(10000000L);
         assertThat(orderNumber).isLessThanOrEqualTo(19999999L);
+    }
+
+    private Customer defaultCustomer() {
+        Customer customer = new Customer();
+
+        customer.setId(1234L);
+        customer.setName("Thomas");
+
+        return customer;
+    }
+
+    private Menu makiMenu() {
+        Menu menu = new Menu();
+
+        menu.setName("Maki");
+
+        return menu;
     }
 }
