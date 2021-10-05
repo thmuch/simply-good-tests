0391c4dddc77c9fde0dcf868674277a5809446e0
Thomas Much <github@snailshell.de>
2021-10-05 14:53:11 +0200

    Replace value type class by Java 16+ record

--- src/main/java/sushi/model/OrderNumber.java
+++ src/main/java/sushi/model/OrderNumber.java
@@ -1,40 +1,10 @@
 package sushi.model;
 
-import java.util.Objects;
+public record OrderNumber(long eightDigits) {
 
-public final class OrderNumber {
-
-    private final long eightDigits;
-
-    public OrderNumber(long eightDigits) {
+    public OrderNumber {
         if (eightDigits < 10000000L || eightDigits > 19999999L) {
             throw new IllegalArgumentException("Invalid order number: " + eightDigits);
         }
-
-        this.eightDigits = eightDigits;
-    }
-
-    public long eightDigits() {
-        return eightDigits;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        OrderNumber that = (OrderNumber) o;
-        return eightDigits == that.eightDigits;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(eightDigits);
-    }
-
-    @Override
-    public String toString() {
-        return "OrderNumber{" +
-                "eightDigits=" + eightDigits +
-                '}';
     }
 }
